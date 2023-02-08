80d7c16030123683188bb0707e977d82be22eee0
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-02-08 23:25:32 +0100

    Add simple bluetooth test

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+   <div>
+      <h1>Elsys bois</h1>
+   </div>
+   <button @click="connect">Connect</button>
+
+   <p v-if="connectedDevice">Connected to {{ connectedDevice }}</p>
 </template>
 
+<script setup>
+import { ref } from "vue"
+
+const connectedDevice = ref("")
+
+const connect = () => {
+   navigator.bluetooth
+      .requestDevice({
+         acceptAllDevices: true,
+      })
+      .then((device) => {
+         console.log(device)
+         connectedDevice.value = device.name
+      })
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+div {
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   align-content: center;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+button {
+   background-color: white;
+   padding: 5px 10px;
+   cursor: pointer;
 }
 </style>
