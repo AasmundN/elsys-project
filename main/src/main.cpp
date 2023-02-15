d8e890e9a4c4899de3df740dd7678866d2ea5e92
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-02-15 13:32:38 +0100

    Setup freeRTOS

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,10 +1,27 @@
+// libraries
 #include <Arduino.h>
-#include <test.h>
 
+// custom files
+#include <setup.h>
+#include <global.h>
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+// void setup
 void setup() {
-  Serial.begin(115200);
+   // setup Serial
+   Serial.begin(115200); 
+
+   // setup functions
+   setupTasks(Task1, Task2);
+   pinMode(LED, OUTPUT);
+
+   Serial.println("------------------------------------");
+   Serial.println("-            Setup done            -");
+   Serial.println("------------------------------------");
 }
 
 void loop() {
-  test();
+   
 }
