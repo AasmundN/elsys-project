5dd7ff833324afd5539c56742b1019fd8afcfb9b
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-02-15 12:00:12 +0100

    Add platformio setup

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,25 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
 
-createApp(App).use(store).use(router).mount('#app')
+// Vuetify
+import "vuetify/styles"
+import { createVuetify } from "vuetify"
+import * as components from "vuetify/components"
+import * as directives from "vuetify/directives"
+
+const vuetify = createVuetify({
+   components,
+   directives,
+})
+
+const app = createApp(App)
+
+app.use(store)
+app.use(router)
+app.use(vuetify)
+
+router.isReady().then(() => {
+   app.mount("#app")
+})
