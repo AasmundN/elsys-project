a55b3c487d918e27f870d7cb7861bb4e4af34995
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-04-11 15:34:49 +0200

    Remove task 1

--- main/src/setup.cpp
+++ main/src/setup.cpp
@@ -1,6 +1,5 @@
 #include <global.h>
 #include <setup.h>
-#include <task1.h>
 #include <task2.h>
 
 // setup led matrix
@@ -28,7 +27,7 @@ void Task1code( void* pvParameters ) {
       task1();
       // do not remove delay: 
       // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
-      vTaskDelay(1);
+      vTaskDelay(portTICK_PERIOD_MS);
    }
 }
 
@@ -36,7 +35,7 @@ void Task2code( void* pvParameters ) {
    Serial.print("Task2 running on core ");
    Serial.println(xPortGetCoreID());
 
-   // serup I2S sampler
+   // setup I2S sampler
    sampler = (I2SSampler *)pvParameters;
    samples = (int16_t *)malloc(sizeof(uint16_t) * SAMPLE_SIZE);
 
@@ -47,35 +46,25 @@ void Task2code( void* pvParameters ) {
 
    for(;;) {
       // task 2 code
+      // Serial.println("Task 2 running");
       task2();
       // do not remove delay: 
       // https://stackoverflow.com/questions/66278271/task-watchdog-got-triggered-the-tasks-did-not-reset-the-watchdog-in-time
-      vTaskDelay(1);
+      vTaskDelay(10/portTICK_PERIOD_MS);
    }
 }
 
-void setupTasks(TaskHandle_t Task1, TaskHandle_t Task2) {
-
-   //create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
-   xTaskCreatePinnedToCore(
-                  Task1code,   /* Task function. */
-                  "Task1",     /* name of task. */
-                  10000,       /* Stack size of task */
-                  NULL,        /* parameter of the task */
-                  1,           /* priority of the task */
-                  &Task1,      /* Task handle to keep track of created task */
-                  0);          /* pin task to core 0 */                  
-   delay(500); 
+void setupTasks(TaskHandle_t Task2) {
 
    //create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
    xTaskCreatePinnedToCore(
                   Task2code,   /* Task function. */
                   "Task2",     /* name of task. */
-                  10000,       /* Stack size of task */
-                  adcSampler,        /* parameter of the task */
+                  1000,       /* Stack size of task */
+                  adcSampler,  /* parameter of the task */
                   1,           /* priority of the task */
                   &Task2,      /* Task handle to keep track of created task */
-                  1);          /* pin task to core 1 */
+                  0);          /* pin task to core 1 */
    delay(500); 
 
    Serial.println("Task setup done");
